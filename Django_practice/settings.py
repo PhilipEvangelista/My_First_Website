406a97709c148b2b353c6d1c82115f7f3df7ba1c
PhilipEvangelista <100225804+PhilipEvangelista@users.noreply.github.com>
2022-02-24 01:18:34 +0800

    commit

--- Django_practice/settings.py
+++ Django_practice/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f!3(2y1s016gv1%avy5=ed44u484ht)du)255-e-5s38vu4gin
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'deptdatabase.herokuapp.com']
 
 
 # Application definition
