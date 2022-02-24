52dcaa62c1f0bdc4b45407ab3c1b200699c3d63d
PhilipEvangelista <100225804+PhilipEvangelista@users.noreply.github.com>
2022-02-24 13:10:40 +0800

    Update settings.py

--- Django_practice/settings.py
+++ Django_practice/settings.py
@@ -15,11 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'base\\static')
 
 
 # Quick-start development settings - unsuitable for production
